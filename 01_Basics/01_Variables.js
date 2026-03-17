3094cc7e2292154c09ed718d227acd0d93fdcb4c
VINAYAK BHARDWAJ <bhardwajvinayak386@gmail.com>
2026-03-17 09:40:45 +0000

    variabels

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,21 @@
+const acountId = 1234567
+let email="vinayak@gmail.com"
+var acountPassword="2313"
+acountCity="Pune"
+
+//  acountId=1     //  not allowed because accountId is a constant
+ 
+email="jcdnsj@gmAIL.COM"
+ acountPassword="1223"
+acountCity="Mumbai"
+let acountState;
+// console.log(acountId)
+// console.log(email)
+// console.log(acountPassword)
+// console.log(acountCity)     
+
+console.table([acountId,email,acountPassword,acountCity,acountState])
+/*
+Prefer not use var
+because of issues with Block scope and Function scope
+*/
