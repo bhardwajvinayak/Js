20eadf43ddf0953699c30f4268db1fe769ae1e3f
VINAYAK BHARDWAJ <bhardwajvinayak386@gmail.com>
2026-03-17 10:56:31 +0000

    problems in  null and undefined   to any data  coparision

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(1 > 2)
+// console.log(1 < 2)
+// console.log(1 >= 2)
+// console.log(1 <= 2)
+// console.log(1 == 2)
+// console.log(1 != 2)
+
+// console.log(1 == "1") // true
+// console.log(1 === "1") // false
+// console.log(1 != "1") // false
+// console.log(1 !== "1") // true   
+
+//console.log(null > 0) // false
+//console.log(null == 0) // false
+//console.log(null >= 0) // true\
+
+//console.log(undefined > 0) // false
+//console.log(undefined == 0) // false
+//console.log(undefined >= 0) // false;
+
+
+// (strict check )  use this keyword  ===
+
+console.log(null === 0);
