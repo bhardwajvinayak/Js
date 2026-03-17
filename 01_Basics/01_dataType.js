a351aa628f788ba8aa177ce7f8145ade71191ff3
VINAYAK BHARDWAJ <bhardwajvinayak386@gmail.com>
2026-03-17 09:41:05 +0000

    dataTypes

---
+++ 01_Basics/01_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; // treats all js code as modern js code and avoid some of the old mistakes
+
+alert(1 + 2) // we use nodejs ,not a browser
+
+console.log(1 +
+    2) // code readeblit should be  high
+
+    console.log("vinayak")
+
+    // number =>  2 to 53
+    // bigint
+    // string => "vinayak"  'vinayak'  `vinayak`
+    // boolean => true or false
+    // null => empty value
+    // undefined => value is not assigned
+    // symbol => unique identifier
+
+
+    // object
+
+    console.log(typeof 123) // number
+    console.log(typeof undefined) // o/p => undefined
+    console.log(typeof null) // o/p => object (bug in js)
