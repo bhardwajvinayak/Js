e9cf299ddc599852d0e625aea94c13f318aec6c7
VINAYAK BHARDWAJ <bhardwajvinayak386@gmail.com>
2026-04-01 18:22:19 +0000

    disscuss about  switchstatement and  break statement

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,45 @@
+// switch statement
+
+
+// syntax of switch statement
+
+// switch(expression){
+//     case value1:
+//         // code to be executed if expression === value1
+//         break;
+//     case value2:
+//         // code to be executed if expression === value2
+//         break;
+//     default:
+//         // code to be executed if expression doesn't match any case
+// }
+
+// example of switch statement
+
+//   const month = 3;
+  
+//   switch(month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");               
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");                         
+//         break; 
+//         default : console.log("Invalid month");
+
+//     } 
+
+     // break is not write in code then it will execute all case except default case 
+    // because of fall through behavior of switch statement.   
+    
+    
+    
