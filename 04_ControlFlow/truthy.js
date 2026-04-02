c73fd7107c8f91a3de71cbf2ed2ccce296e1559f
VINAYAK BHARDWAJ <bhardwajvinayak386@gmail.com>
2026-04-02 16:44:21 +0000

    loops ,continue and break statement

---
+++ 04_ControlFlow/truthy.js
@@ -0,0 +1,13 @@
+ // Truthy Value
+
+   //a truthy value is a value that is considered true when encountered in a Boolean context.
+
+  // example -:- true , "0", ' ', 1 , -1 , [] , {} , function(){}
+ 
+  
+ // Falsy Value 
+
+  // a falsy value is a value that is considered false when encountered in a Boolean context.
+
+  // example -:- false , "", '' , 0 , -0 , null , undefined ,NaN , BigInt 0n
+
